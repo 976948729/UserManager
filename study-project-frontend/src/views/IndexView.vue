b789ee1d95b921a72bfb5bf96830ccfec39971c1
f163 <f976948729@163.com>
2023-05-13 15:08:59 +0800

    基本路由配置

---
+++ study-project-frontend/src/views/IndexView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    欢迎进入学习平台
+  </div>
+  <div>
+    <el-button type="danger" plain>退出登录</el-button>
+  </div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
